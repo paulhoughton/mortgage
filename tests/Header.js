78cee7949039a2106252da73b194d9890acc50c8
paulhoughton <paulhoughton@users.noreply.github.com>
2015-10-10 08:19:37 +0100

    Formatting

--- tests/Header.js
+++ tests/Header.js
@@ -11,12 +11,12 @@ function createComponent(component, props, ...children) {
 }
 
 test("Header navbar test", function (assert) {
-  const headerComponent = createComponent(Header, {title:"The Title"});
-  const container=headerComponent.props.children;
-  assert.equal(headerComponent.type, "nav", "should be a nav");
-  const navbar=container.props.children;
-  const brand=navbar.props.children;
-  assert.equal(brand.props.children, "The Title", "should display supplied title");
+	const headerComponent = createComponent(Header, {title:"The Title"});
+	const container=headerComponent.props.children;
+	assert.equal(headerComponent.type, "nav", "should be a nav");
+	const navbar=container.props.children;
+	const brand=navbar.props.children;
+	assert.equal(brand.props.children, "The Title", "should display supplied title");
 	assert.end();
 });
 
