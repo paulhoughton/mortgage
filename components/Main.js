60e7aafa06dfde5c32b25de43054c9ab330616d1
pahoughton@gmail.com <pahoughton@gmail.com>
2015-10-11 14:11:11 +0100

    Responsive fix

--- components/Main.js
+++ components/Main.js
@@ -13,21 +13,21 @@ export default class MainSection extends Component {
     
     return (
       <div className="container-fluid">
-        <div className="col-md-8">
-          <div className="col-md-4">
+        <div className="col-md-8 col-sm-12">
+          <div className="col-sm-4">
             <BaseFigures {...this.props} />
           </div>
-          <div className="col-md-8">
+          <div className="col-sm-8">
             <Overpayment {...this.props} />
           </div>
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <h2>Monthly Payment:
               <span className="money"> {(+monthlyPayment + monthlyOverpayment).toFixed(2)}</span>
             </h2>
             <Chart data={payments} />
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-sm-4">
           <Table data={payments}/>
         </div>
       </div>
