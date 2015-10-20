d93f4ffadef66c5cc7c301358014da4a7ebbf859
paulhoughton <paulhoughton@users.noreply.github.com>
2015-10-20 18:39:13 +0100

    Animate

--- components/Chart.js
+++ components/Chart.js
@@ -6,6 +6,31 @@ const margin = {top: 20, right: 20, bottom: 30, left: 75},
 	fullHeight = 300,
 	width = fullWidth - margin.left - margin.right,
 	height = fullHeight - margin.top - margin.bottom;
+	
+const ANIM_SPEED=250;
+
+const x= d3.scale.linear()
+			.range([0, width]);
+
+const y = d3.scale.linear()
+			.range([height, 0]);
+			
+const line = d3.svg.line()
+			.x((d,i)=>x(i+((d.partial/12)||1)-1))
+			.y(d=>y(d.balance));
+
+const baseline = d3.svg.line()
+			.x((d,i)=>x(i))
+			.y(d=>y(d.baseline));		
+
+const xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+const yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left");
+
 
 export default class Chart extends Component {
 
@@ -13,8 +38,28 @@ export default class Chart extends Component {
 		return (<svg ref="chart"></svg>)
 	}
 
-	shouldComponentUpdate(props){
-		this.redraw.call(this, props);
+	shouldComponentUpdate({data}){
+		  x.domain([0,data.length-1])
+		  y.domain([0,data[0].balance]);
+		  xAxis.ticks(Math.min(data.length,30))
+
+		var svg=d3.select(this.refs.chart).select("g").transition();
+        svg.select(".overpayline")
+            .duration(ANIM_SPEED)
+            .attr("d", line(data));
+
+        svg.select(".baseline")
+            .duration(ANIM_SPEED)
+            .attr("d", baseline(data));
+
+        svg.select(".x.axis")
+            .duration(ANIM_SPEED)
+            .call(xAxis);
+			
+        svg.select(".y.axis")
+            .duration(ANIM_SPEED)
+            .call(yAxis);
+
 		return false;
 	}
 
@@ -26,60 +71,34 @@ export default class Chart extends Component {
 			.attr('preserveAspectRatio','xMidYMid')
 			.append("g")
 			.attr("transform", `translate(${margin.left},${margin.top})`);
-			
-		this.redraw.call(this, this.props);
-	}	
-	
-	redraw({data}) {
+
+		var {data} = this.props;
+
 		var svg = d3.select(this.refs.chart).select("g");
+		
+		
+		x.domain([0,data.length-1])
+		y.domain([0,data[0].balance]);
+		xAxis.ticks(Math.min(data.length,30))
 
-		var x = d3.scale.linear()
-			.range([0, width]);
-
-		var y = d3.scale.linear()
-			.range([height, 0]);
-
-		var xAxis = d3.svg.axis()
-			.scale(x)
-			.ticks(Math.min(data.length,30))
-			.orient("bottom");
-
-		var yAxis = d3.svg.axis()
-			.scale(y)
-			.orient("left");
-
-		var line = d3.svg.line()
-			.x((d,i)=>x(i+((d.partial/12)||1)-1))
-			.y(d=>y(d.balance));
-			
-		var baseline = d3.svg.line()
-			.x((d,i)=>x(i))
-			.y(d=>y(d.baseline));
-
-		  x.domain([0,data.length-1])
-		  y.domain([0,data[0].balance]);
-
-		  svg.selectAll(".axis").remove();
-		  svg.selectAll(".line").remove();
-		  
-		  svg.append("g")
-			  .attr("class", "x axis")
-			  .attr("transform", `translate(0,${height})`)
-			  .call(xAxis);
-
-		  svg.append("g")
-			  .attr("class", "y axis")
-			  .call(yAxis)
-				  
-		  svg.append("path")
-			  .datum(data)
-			  .attr("class", "line")
-			  .style("stroke-dasharray", ("4, 4"))
-			  .attr("d", baseline);
-
-		  svg.append("path")
-			  .datum(data)
-			  .attr("class", "line")
-			  .attr("d", line);
-	}
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", `translate(0,${height})`)
+			.call(xAxis);
+		
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+				
+		svg.append("path")
+			.datum(data)
+			.attr("class", "line baseline")
+			.style("stroke-dasharray", ("4, 4"))
+			.attr("d", baseline);
+		
+		svg.append("path")
+			.datum(data)
+			.attr("class", "line overpayline")
+			.attr("d", line);
+	}	
 };
