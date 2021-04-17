ba150a865389bc74fa52b35b76cc4da877bbaef3
TylerNguyen21 <tydnguyen21@gmail.com>
2021-04-17 14:53:16 -0700

    hooked up project to listen to LD flag features asynchornouslly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
+
+(async () => {
+  const LDProvider = await asyncWithLDProvider({
+    clientSideID: '607a030de00b830bbb2020e5',
+    user: {
+      "key": "Actual-chart",
+      "name": "Tyler Nguyen",
+      "email": "tydnguyen@ucdavis.edu"
+    },
+    options: { /* ... */ }
+  });
+
+  ReactDom.render(
+    <LDProvider>
+      <App />
+    </LDProvider>,
+    document.getElementById('root'),
+  );
+})();
