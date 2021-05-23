63fa975833b79ba746fc8a9682354b1997e7a0d8
Sachini-Dissanayaka <dmsachiniacc@gmail.com>
2021-05-23 09:28:19 +0530

    Error Prevention - validating and error handling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventIcon from '@material-ui/icons/Event';
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
+import React, {useState} from 'react';
 
 import './App.css';
 
@@ -11,152 +12,161 @@ import Table from './Table';
 import Chart from './Chart';
 import calculate from './calculations';
 
-const defaultOverpayment = { month: '1', year: '0', amount: '0' };
+const defaultOverpayment = {month: '1', year: '0', amount: '0'};
 
 export default () => {
-  const [initial, setInitial] = useState('200000');
-  const [rate, setRate] = useState('5');
-  const [years, setYears] = useState('25');
-  const [monthlyOverpayment, setMonthlyOverpayment] = useState('0');
-  const [overpayments, setOverpayments] = useState([defaultOverpayment]);
+    const [initial, setInitial] = useState('200000');
+    const [rate, setRate] = useState('5');
+    const [years, setYears] = useState('25');
+    const [monthlyOverpayment, setMonthlyOverpayment] = useState('0');
+    const [overpayments, setOverpayments] = useState([defaultOverpayment]);
 
-  const updateOverpayment = index => ({ target }) =>
-    setOverpayments(
-      overpayments.map((overpayment, i) =>
-        i === index
-          ? { ...overpayment, [target.name]: target.value }
-          : overpayment
-      )
+    const updateOverpayment = index => ({target}) =>
+        setOverpayments(
+            overpayments.map((overpayment, i) =>
+                i === index
+                    ? {...overpayment, [target.name]: target.value}
+                    : overpayment
+            )
+        );
+
+    const {monthlyPayment, payments} = calculate(
+        +initial,
+        +years,
+        +rate,
+        +monthlyOverpayment,
+        overpayments
     );
 
-  const { monthlyPayment, payments } = calculate(
-    +initial,
-    +years,
-    +rate,
-    +monthlyOverpayment,
-    overpayments
-  );
+    return (
+        <div>
+            <nav className="navbar navbar-default">
+                <div className="navbar-header">
+                    <div className="navbar-brand" style={{fontFamily: 'Times New Roman', fontSize: 25}}>
+                        Mortgage Overpayment Calculator
+                    </div>
+                </div>
+            </nav>
+            <div className="container-fluid">
+                <div className="col-md-8 col-sm-12">
+                    <div className="col-sm-4">
+                        <div>
+                            <h2>Initial</h2>
+                            <label>
+                                <MonetizationOnIcon className= {'material-icons'} />
+                                Amount
+                            </label>
+                            <input
+                                type="number"
+                                maxLength={7}
+                                value={initial}
+                                onChange={e => {
+                                    e.target.value = !!e.target.value && Math.abs(e.target.value) >= 0 ? Math.abs(e.target.value) : null;
+                                    setInitial(e.target.value)}}
+                            />
+                        </div>
+                        <div>
+                            <label>
+                                <EventIcon className= {'material-icons'} />
+                                Years
+                            </label>
+                            <input
+                                type="number"
+                                maxLength={2}
+                                value={years}
+                                onChange={e => {
+                                    e.target.value = !!e.target.value && Math.abs(e.target.value) >= 0 ? Math.abs(e.target.value) : null;
+                                    setYears(e.target.value)}}
+                            />
+                        </div>
+                        <div>
+                            <label>Rate</label>
+                            <input
+                                type="number"
+                                step={0.1}
+                                value={rate}
+                                onChange={e => {
+                                    e.target.value = !!e.target.value && Math.abs(e.target.value) >= 0 ? Math.abs(e.target.value) : null;
+                                    setRate(e.target.value)}}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-sm-8">
+                        <div>
+                            <h2>Overpayment</h2>
+                            <label>Monthly</label>
+                            <input
+                                type="number"
+                                maxLength={5}
+                                value={monthlyOverpayment}
+                                onChange={e => {
+                                    e.target.value = !!e.target.value && Math.abs(e.target.value) >= 0 ? Math.abs(e.target.value) : null;
+                                    setMonthlyOverpayment(e.target.value)}}
+                            />
+                        </div>
+                        <div>
+                            <label>Year</label>
+                            <label>Month</label>
+                            <label>
+                                <MonetizationOnIcon className= {'material-icons'} />
+                                Amount
+                            </label>
+                        </div>
+                        {overpayments.map(({year, month, amount}, i) => (
+                            <div key={i}>
+                                <input
+                                    type="number"
+                                    min="0"
+                                    max={years}
+                                    value={year}
+                                    name="year"
+                                    onChange={updateOverpayment(i)}
+                                />
+                                <input
+                                    type="number"
+                                    min="1"
+                                    max="12"
+                                    value={month}
+                                    name="month"
+                                    onChange={updateOverpayment(i)}
+                                />
+                                <input
+                                    type="number"
+                                    value={amount}
+                                    name="amount"
+                                    onChange={updateOverpayment(i)}
+                                />
 
-  return (
-    <div>
-      <nav className="navbar navbar-default">
-        <div className="navbar-header">
-          <div className="navbar-brand" style={{fontFamily: 'Times New Roman', fontSize: 25}}>
-            Mortgage Overpayment Calculator
-          </div>
-        </div>
-      </nav>
-      <div className="container-fluid">
-        <div className="col-md-8 col-sm-12">
-          <div className="col-sm-4">
-            <div>
-              <h2>Initial</h2>
-              <label>
-                <MonetizationOnIcon className= {'material-icons'} />
-                Amount
-              </label>
-              <input
-                maxLength={7}
-                value={initial}
-                onChange={e => setInitial(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>
-                <EventIcon className= {'material-icons'} />
-                Years
-              </label>
-              <input
-                type="number"
-                maxLength={2}
-                value={years}
-                onChange={e => setYears(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>Rate</label>
-              <input
-                type="number"
-                step={0.1}
-                value={rate}
-                onChange={e => setRate(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="col-sm-8">
-            <div>
-              <h2>Overpayment</h2>
-              <label>Monthly</label>
-              <input
-                type="number"
-                maxLength={5}
-                value={monthlyOverpayment}
-                onChange={e => setMonthlyOverpayment(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>Year</label>
-              <label>Month</label>
-              <label>
-                <MonetizationOnIcon className= {'material-icons'} />
-                Amount
-              </label>
-            </div>
-            {overpayments.map(({ year, month, amount }, i) => (
-              <div key={i}>
-                <input
-                  type="number"
-                  min="0"
-                  max={years}
-                  value={year}
-                  name="year"
-                  onChange={updateOverpayment(i)}
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="12"
-                  value={month}
-                  name="month"
-                  onChange={updateOverpayment(i)}
-                />
-                <input
-                  type="text"
-                  value={amount}
-                  name="amount"
-                  onChange={updateOverpayment(i)}
-                />
-
-                {i === overpayments.length - 1 ? (
-                  <IconButton  color="secondary" variant="outlined"
-                  onClick={() =>
-                    setOverpayments([...overpayments, defaultOverpayment])
-                  }>
-                  <AddIcon />
-                </IconButton>
-                ) : (
-                   <IconButton  color="secondary" variant="outlined"
-                   onClick={() =>
-                    setOverpayments(overpayments.filter((_, j) => j !== i))
-                  }>
-                   <DeleteIcon />
-                 </IconButton>
-                )}
-              </div>
-            ))}
-          </div>
-          <div className="col-sm-12">
-            <h2>
-              Monthly Payment
-              <span className="money">
+                                {i === overpayments.length - 1 ? (
+                                    <IconButton  color="secondary" variant="outlined"
+                                                 onClick={() =>
+                                                     setOverpayments([...overpayments, defaultOverpayment])
+                                                 }>
+                                        <AddIcon />
+                                    </IconButton>
+                                ) : (
+                                    <IconButton  color="secondary" variant="outlined"
+                                                 onClick={() =>
+                                                     setOverpayments(overpayments.filter((_, j) => j !== i))
+                                                 }>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="col-sm-12">
+                        <h2>
+                            Monthly Payment
+                            <span className="money">
                 {(+monthlyOverpayment + monthlyPayment).toFixed(2)}
               </span>
-            </h2>
-            <Chart payments={payments} />
-          </div>
+                        </h2>
+                        <Chart payments={payments}/>
+                    </div>
+                </div>
+                <Table className="col-sm-4" payments={payments}/>
+            </div>
         </div>
-        <Table className="col-sm-4" payments={payments} />
-      </div>
-    </div>
-  );
+    );
 };
