fc48b34f3327d2bb42400b592f2efc962c629631
josephprasadula <josephprasadula@gmail.com>
2022-07-25 15:48:25 +0530

    added stying to table, and restricted the number of payment years to 45

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
